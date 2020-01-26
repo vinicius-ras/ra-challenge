ada5197ba85c8f3f787ecc71e2d1099e67540a5f
Vinicius Rogério Araujo Silva <31182306+vinicius-ras@users.noreply.github.com>
2020-01-26 14:09:24 -0300

    * Created initial structure for the project using Spring Boot (dependencies: Spring Web + Spring Data MongoDB + Lombok).

---
+++ src/main/java/br/com/viniciusras/rachallenge/RaChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.viniciusras.rachallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/viniciusras/rachallenge/RaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viniciusras.rachallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
