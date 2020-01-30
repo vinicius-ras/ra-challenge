7715299f5977be39f7c14781903e62ff7876c4d0
Vinicius Rogério Araujo Silva <31182306+vinicius-ras@users.noreply.github.com>
2020-01-30 20:02:07 +0000

    * Updated the docker-compose file for the vscode development container in order for the ReactJS Web App to share the development files with the container's instance, allowing for live updates to take place. * Configured the Web App to use TailwindCSS, Autoprefixer, and PostCSS. * Added custom build configurations the "package.json" file.

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/compiled-styles.css';
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload xx.
         </p>
         <a
           className="App-link"
